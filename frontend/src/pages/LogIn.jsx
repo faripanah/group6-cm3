e2735a3fc1d92c60de398b6459660d7a08c0b3fd
faripanah <farnaz.jamilpanah@metropolia.fi>
2025-03-07 16:55:26 +0200

    last updated for deploy

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -27,7 +27,7 @@ const Login = () => {
         <input {...username} />
         <label>Password:</label>
         <input {...password} />
-        <button>Sign up</button>
+        <button>Sign-up</button>
       </form>
     </div>
   );
