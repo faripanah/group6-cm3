8c1b66ecd2c24a5b2657a1b4b2d8fec4771377b8
faripanah <farnaz.jamilpanah@metropolia.fi>
2025-03-07 08:53:12 +0200

    updated JobListing

--- frontend/src/components/JobListing.jsx
+++ frontend/src/components/JobListing.jsx
@@ -7,7 +7,10 @@ const JobListing = ({ job }) => {
       <p>Type: {job.type}</p>
       <p>Description: {job.description}</p>
       <p>Company: {job.company.name}</p>
-      <Link to={`/jobs/${job.id}`}  className="btn">View Job</Link> {/* Add this line */}
+      <p>Location: {job.location}</p>
+      <p>Salary: {job.salary}</p>
+      <p>Number of employee: {job.size}</p>
+      <Link to={`/jobs/${job.id}`}  className="btn">View Job</Link> 
       <Link to={`/edit-job/${job.id}`} className="btn">Edit Job</Link>
     </div>
   );
